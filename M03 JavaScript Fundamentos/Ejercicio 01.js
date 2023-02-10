b48d6ca2b6a7633826903e7af9361879c3d6fbec
Salmos234 <navarronahuelezequiel@gmail.com>
2023-02-10 12:41:47 -0300

    probando

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = null1234;
+const nuevoString = null; Nahuel
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
